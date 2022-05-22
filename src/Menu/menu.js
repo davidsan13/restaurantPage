9b753af9004a859daad531f97224d7051b1f88e0
davidsan13 <sandavid08@yahoo.com>
2022-05-21 20:06:45 -0700

    added eslint and prettier to project

--- src/Menu/menu.js
+++ src/Menu/menu.js
@@ -1,58 +1,57 @@
 import json from './menu.json5';
 
+var a = 4;
 
 function createMenuMain() {
-   
-    const main = document.querySelector("main");
-    const h1 = document.createElement("h1");
-    // const itemsCont = document.createElement("div");
+  const main = document.querySelector('main');
+  const h1 = document.createElement('h1');
+  // const itemsCont = document.createElement("div");
 
-    main.classList.add("menu");
-    h1.classList.add("title");
-    // itemsCont.classList.add("container");
+  main.classList.add('menu');
+  h1.classList.add('title');
+  // itemsCont.classList.add("container");
 
-    h1.textContent = "Menu";
+  h1.textContent = 'Menu';
 
-    // itemsCont.appendChild(createCard());
+  // itemsCont.appendChild(createCard());
 
-    main.appendChild(h1);
-    main.appendChild(createCard())
+  main.appendChild(h1);
+  main.appendChild(createCard());
 
-    console.log("hello")
-    return main
+  console.log('hello');
+  return main;
 }
 
 function createCard() {
-    const itemsCont = document.createElement("div");
-    const items = json.menu;
-    items.forEach((item) => {
-        let div = document.createElement("div");
-        const title = document.createElement("h1");
-        const price = document.createElement("h1");
+  const itemsCont = document.createElement('div');
+  const items = json.menu;
+  items.forEach((item) => {
+    const div = document.createElement('div');
+    const title = document.createElement('h1');
+    const price = document.createElement('h1');
 
-        div.classList.add("card");
-        title.classList.add("item");
-        price.classList.add("price");
+    div.classList.add('card');
+    title.classList.add('item');
+    price.classList.add('price');
 
-        const getTitle = item.item;
-        const getPrice = item.price;
+    const getTitle = item.item;
+    const getPrice = item.price;
 
-        title.textContent = getTitle;
-        price.textContent = getPrice;
+    title.textContent = getTitle;
+    price.textContent = getPrice;
 
-        div.appendChild(title);
-        div.appendChild(price);
+    div.appendChild(title);
+    div.appendChild(price);
 
-        itemsCont.appendChild(div);
-        
-    })
-    return itemsCont
+    itemsCont.appendChild(div);
+  });
+  return itemsCont;
 }
 
 function createPage() {
-    const homeMain = document.querySelector("main")
-    homeMain.textContent = "";
-    createMenuMain()
+  const homeMain = document.querySelector('main');
+  homeMain.textContent = '';
+  createMenuMain();
 }
 
 export default createPage;
--- src/componets/layout.js
+++ src/componets/layout.js
@@ -1,72 +1,67 @@
-import createPage from "../Menu/menu.js"
+import createPage from '../Menu/menu';
 
 function createHeader() {
-    const header = document.createElement("header");
-    header.classList.add("header");
+  const header = document.createElement('header');
+  header.classList.add('header');
 
-    const name = document.createElement("h1");
-    name.classList.add("name");
-    name.textContent = "Puzzle";
+  const name = document.createElement('h1');
+  name.classList.add('name');
+  name.textContent = 'Puzzle';
 
-    header.appendChild(name);
-    header.appendChild(createNav());
-    
-    return header;
+  header.appendChild(name);
+  header.appendChild(createNav());
+
+  return header;
 }
 
 function createNav() {
-    const nav = document.createElement("nav");
-    const home = document.createElement("div");
-    const menu = document.createElement("div");
-    const contact = document.createElement("div");
+  const nav = document.createElement('nav');
+  const home = document.createElement('div');
+  const menu = document.createElement('div');
+  const contact = document.createElement('div');
 
-    nav.classList.add("nav")
-    home.classList.add("hom")
-    menu.classList.add("menu")
-    contact.classList.add("contact")
+  nav.classList.add('nav');
+  home.classList.add('hom');
+  menu.classList.add('menu');
+  contact.classList.add('contact');
 
-    home.textContent = "Home";
-    menu.textContent = "Menu";
-    contact.textContent = "Contact";
+  home.textContent = 'Home';
+  menu.textContent = 'Menu';
+  contact.textContent = 'Contact';
 
-    nav.appendChild(home);
-    nav.appendChild(menu);
-    nav.appendChild(contact);
+  nav.appendChild(home);
+  nav.appendChild(menu);
+  nav.appendChild(contact);
 
-    
-   
-    menu.addEventListener("click", (e) => {
-      if (e.target.classList.contains("active")) return;
-      createPage();
-    });
+  menu.addEventListener('click', (e) => {
+    if (e.target.classList.contains('active')) return;
+    createPage();
+  });
 
-    return nav;
-
-
+  return nav;
 }
 function createMain() {
-    const main = document.createElement("main");
-    const h1 = document.createElement("h1");
-    const p = document.createElement("p");
+  const main = document.createElement('main');
+  const h1 = document.createElement('h1');
+  const p = document.createElement('p');
 
-    main.classList.add("main")
-    h1.textContent = "Puzzle";
-    p.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+  main.classList.add('main');
+  h1.textContent = 'Puzzle';
+  p.textContent =
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
-    main.appendChild(h1);
-    main.appendChild(p);
-    return main;
-
+  main.appendChild(h1);
+  main.appendChild(p);
+  return main;
 }
 
 function addListener() {
-    const navBtn = document.querySelector(".menu")
+  const navBtn = document.querySelector('.menu');
 
-    navBtn.addEventListener("click", createPage())
+  navBtn.addEventListener('click', createPage());
 
+  console.log(navBtn);
 }
-function createFooter() {
+function createFooter() { }
 
-}
-
-export {createHeader, createMain, addListener} ;
+export { createHeader, createMain, addListener };
